9a6fc041b42294b6aa0c7389513bcfc8377d1d1a
Prathamesh <enggtutes@gmail.com>
2022-08-18 16:10:20 +0530

    first commit

---
+++ output/TabsAndNewLine.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n\tHello everyone\n\t\t My name is Prathamesh" << endl;
+    cout << "I am learning CPP" << endl
+         << "\tI am very excited to create new softwares";
+}
